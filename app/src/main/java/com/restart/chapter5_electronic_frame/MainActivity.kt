9406a7836c2328b0cf7c5d824217aa71ba57b302
ujin-Bang <91653684+ujin-Bang@users.noreply.github.com>
2022-07-29 17:40:05 +0900

    전자액자 ui 그리기 + 기본 설정

--- app/src/main/java/com/restart/chapter5_electronic_frame/MainActivity.kt
+++ app/src/main/java/com/restart/chapter5_electronic_frame/MainActivity.kt
@@ -2,10 +2,31 @@ package com.restart.chapter5_electronic_frame
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+
+    private val addPhotoButton:Button by lazy {
+        findViewById(R.id.addPhotoButton)
+    }
+
+    private val startPhotoFramModeButton: Button by lazy {
+        findViewById(R.id.startPhotoFrameModeButton)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        initAddPhotoButton()
+        initStartPhotoFrameModeButton()
+    }
+
+    private fun initAddPhotoButton(){
+
+    }
+
+    private fun initStartPhotoFrameModeButton(){
+
     }
 }
