c38df8ffdffc0cb2c1d75529bb7f14bf2b56ef79
ujin-Bang <91653684+ujin-Bang@users.noreply.github.com>
2022-07-29 17:03:12 +0900

    Initial commit

---
+++ app/src/main/java/com/restart/chapter5_electronic_frame/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.restart.chapter5_electronic_frame
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
