60287e246837788ba395f2c89269ed53806b3c77
ujin-Bang <91653684+ujin-Bang@users.noreply.github.com>
2022-07-30 13:26:24 +0900

    사진 추가하기 작업중  - 핸드폰에 저장되어 있는 이미지 선택해서 앱에 반영  - 이미지 가져오기 SAF기능  - 콜백함수 onActivitiResult 구현  - 이미지리스트에 추가, 이미지뷰리스트에 선택한 데이타 반영

--- app/src/main/java/com/restart/chapter5_electronic_frame/MainActivity.kt
+++ app/src/main/java/com/restart/chapter5_electronic_frame/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.restart.chapter5_electronic_frame
 
+import android.app.Activity
 import android.content.DialogInterface
 import android.content.Intent
 import android.content.pm.PackageManager
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
@@ -33,6 +35,8 @@ class MainActivity : AppCompatActivity() {
             add(findViewById(R.id.imageView23))
         }
     }
+
+    private val imageUriList: MutableList<Uri> =  mutableListOf()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -65,7 +69,7 @@ class MainActivity : AppCompatActivity() {
 
     }
 
-    //권한이 부여되었을 때 결과값 받아오는 함수.
+    //권한요청 => requestPermssions 콜백함수.
     override fun onRequestPermissionsResult(
         requestCode: Int,
         permissions: Array<out String>,
@@ -88,11 +92,45 @@ class MainActivity : AppCompatActivity() {
             }
         }
     }
-    private fun navigatePhotos(){
+
+    //SAF기능으로 이미지 가져오기
+   private fun navigatePhotos(){
         val intent = Intent(Intent.ACTION_GET_CONTENT)
         intent.type = "image/*"
-        startActivityForResult(intent, 2000)
+        startActivityForResult(intent, 2000) //이미지를 가지고 돌아오기. startActivityForResult
     }
+
+    //startActivityForResult 콜백함수 : 이미지 가지러 갔다와서 실행하는 함수.
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (resultCode != Activity.RESULT_OK){ //이미지 선택을 하지 않고, 백버튼을 누르거나 앱을 종료시킨 경우등 RESULT_OK가 되지 않은 상태이다.
+            return
+        }
+        when(requestCode){
+            2000 -> { //리퀘스트 코드가 2000번이 맞다면 데이타를 Uri변수에 저장
+                val selectedImageUri: Uri? = data?.data
+
+                if (selectedImageUri != null){
+
+                    if (imageUriList.size == 6){ //6개까지만 보여주도록 UI를 작성했기 때문
+                        Toast.makeText(this, "최대 6개까지 선택가능합니다.", Toast.LENGTH_SHORT).show()
+                        return
+                    }
+                    imageUriList.add(selectedImageUri) //선택한 이미지 리스트에 추가
+                    imageViewList[imageUriList.size -1].setImageURI(selectedImageUri)//이미지뷰에 선택한 이미지Uri로 세팅 리스트사이즈 -1번째부터
+                }
+                else {
+                    Toast.makeText(this, "사진을 가져오지 못했습니다.", Toast.LENGTH_SHORT).show()
+                }
+
+            }
+            else -> {
+                Toast.makeText(this, "사진을 가져오지 못했습니다.", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
     private fun showPermissionContextPopup(){
         AlertDialog.Builder(this)
             .setTitle("권한이 필요합니다.")
