459c37739842d55fcc2ca8cc10265db65117d44b
ujin-Bang <91653684+ujin-Bang@users.noreply.github.com>
2022-07-30 11:36:29 +0900

    사진 추가하기 작업중  - 권한설정 예외처리 => 내 핸드폰에 접근까지

--- app/src/main/java/com/restart/chapter5_electronic_frame/MainActivity.kt
+++ app/src/main/java/com/restart/chapter5_electronic_frame/MainActivity.kt
@@ -1,10 +1,13 @@
 package com.restart.chapter5_electronic_frame
 
 import android.content.DialogInterface
+import android.content.Intent
 import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
+import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
 import androidx.core.content.ContextCompat
 import androidx.core.content.PackageManagerCompat
@@ -17,6 +20,18 @@ class MainActivity : AppCompatActivity() {
 
     private val startPhotoFrameModeButton: Button by lazy {
         findViewById(R.id.startPhotoFrameModeButton)
+    }
+
+    //xml 이미지뷰 리스트에 담아 지연 초기화
+    private val imageViewList: List<ImageView> by lazy {
+        mutableListOf<ImageView>().apply {
+            add(findViewById(R.id.imageView11))
+            add(findViewById(R.id.imageView12))
+            add(findViewById(R.id.imageView13))
+            add(findViewById(R.id.imageView21))
+            add(findViewById(R.id.imageView22))
+            add(findViewById(R.id.imageView23))
+        }
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -36,6 +51,7 @@ class MainActivity : AppCompatActivity() {
                     android.Manifest.permission.READ_EXTERNAL_STORAGE
                 ) == PackageManager.PERMISSION_GRANTED ->{
                     //todo 권한이 잘 부여되었을 때 갤러리에서 사진을 선택하는 기능
+                    navigatePhotos()
                 }
                 shouldShowRequestPermissionRationale(android.Manifest.permission.READ_EXTERNAL_STORAGE) -> {
                     //todo 교육용 팝업 확인 후 권한 팝업을 띄우는 기능
@@ -46,6 +62,36 @@ class MainActivity : AppCompatActivity() {
                 }
             }
         }
+
+    }
+
+    //권한이 부여되었을 때 결과값 받아오는 함수.
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        when(requestCode){
+            1000 -> {
+                if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    //todo 권한이 부여된 경우
+                    navigatePhotos()
+                } else{
+                    Toast.makeText(this, "권한을 거부하셨습니다.", Toast.LENGTH_SHORT).show()
+                }
+
+            }
+            else -> {
+
+            }
+        }
+    }
+    private fun navigatePhotos(){
+        val intent = Intent(Intent.ACTION_GET_CONTENT)
+        intent.type = "image/*"
+        startActivityForResult(intent, 2000)
     }
     private fun showPermissionContextPopup(){
         AlertDialog.Builder(this)
