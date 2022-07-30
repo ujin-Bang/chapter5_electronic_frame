ebbacb38a9298409c9943ec5644a603f42b1d2b7
ujin-Bang <91653684+ujin-Bang@users.noreply.github.com>
2022-07-30 15:40:44 +0900

    putExtra로 넘김 사진데이터 받기 - 포토리스트 사이즈만큼 for문 돌려서 uri값 추출 => string으로 받아온 값 Uri.parse로 Uri타입으로 다시 변환  - getTypeExtra

--- app/src/main/java/com/restart/chapter5_electronic_frame/PhotoFrameActivity.kt
+++ app/src/main/java/com/restart/chapter5_electronic_frame/PhotoFrameActivity.kt
@@ -1,12 +1,27 @@
 package com.restart.chapter5_electronic_frame
 
+import android.net.Uri
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
 class PhotoFrameActivity: AppCompatActivity(){
 
+    private val photoList = mutableListOf<Uri>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_photo_frame)
+
+        getPhotoUriFromIntent()
+
+    }
+
+    private fun getPhotoUriFromIntent(){
+        val size = intent.getIntExtra("photoListSize", 0)
+        for (i in 0 until size){
+            intent.getStringExtra("photo$i")?.let {
+                photoList.add(Uri.parse(it)) //String으로 넘어온 이미지를 다시 Uri타입으로 변환해서 포토리스트에 추가.
+            }
+        }
     }
 }
